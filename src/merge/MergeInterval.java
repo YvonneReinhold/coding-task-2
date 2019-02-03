7406d33878fcfb0313571bcc84d815a47459c1bc
Yvonne Reinhold <47254757+YvonneReinhold@users.noreply.github.com>
2019-02-03 17:30:34 +0100

    Update MergeInterval.java
    Debug-Modus ausgeschaltet

--- src/merge/MergeInterval.java
+++ src/merge/MergeInterval.java
@@ -9,38 +9,38 @@ import merge.util.DebugMemoryAndTime;
  * <p>
  * Aufgabenstellung:<p>
  * Bereitstellen einer MERGE-Funktion, die eine Liste von Intervallen entgegennimmt und als
- * Ergebnis wiederum eine Liste von Intervallen zurückgibt. Im Ergebnis sollen alle sich
- * überlappenden Intervalle gemerged sein. Alle nicht überlappenden Intervalle bleiben unberührt.<br>
+ * Ergebnis wiederum eine Liste von Intervallen zurÃ¼ckgibt. Im Ergebnis sollen alle sich
+ * Ã¼berlappenden Intervalle gemerged sein. Alle nicht Ã¼berlappenden Intervalle bleiben unberÃ¼hrt.<br>
  * Beispiel: Input: [25,30] [2,19] [14, 23] [4,8] -> Output: [2,23] [25,30]
  */
 public class MergeInterval 
 {
-  public static boolean DEBUG_MODE = true;
+  public static boolean DEBUG_MODE = false;
 
   /**
-   * Die Methode führt die folgenden Schritte aus:
+   * Die Methode fÃ¼hrt die folgenden Schritte aus:
    * 
-   * <ol><li>Protokollieren der Startzeit und des belegten Speichers für spätere Auswertungen.</li>
-   *     <li>Übernahme der Befehlszeilenparameter. Diese werden syntaktisch geprüft. Im Fehlerfall wird das betreffende Intervall ignoriert und es wird eine entsprechende Meldung ausgegeben.</li>
+   * <ol><li>Protokollieren der Startzeit und des belegten Speichers fÃ¼r spÃ¤tere Auswertungen.</li>
+   *     <li>Ãœbernahme der Befehlszeilenparameter. Diese werden syntaktisch geprÃ¼ft. Im Fehlerfall wird das betreffende Intervall ignoriert und es wird eine entsprechende Meldung ausgegeben.</li>
    *     <li>Zusammenfassen (mergen) der Intervalldaten.</li>
-   *     <li>Ausgabe der Ergebnisliste, welche die zusammengefassten Intervalle enthält.</li>
+   *     <li>Ausgabe der Ergebnisliste, welche die zusammengefassten Intervalle enthÃ¤lt.</li>
    *     <li>Protokollieren der Endzeit und des belegten Speichers.</li>
-   *     <li>Auswertung der Kennzahlen "Programmlaufzeit" / "benötigter Speicher".</li></ol>
+   *     <li>Auswertung der Kennzahlen "Programmlaufzeit" / "benÃ¶tigter Speicher".</li></ol>
    *     
    * @param intervals Die Intervaldaten werden im Format [\d+,\d+] erwartet. Zwischen den einzelnen Intervallen muss ein Leerzeichen stehen. 
-   *                  Mit dem Parameter <code>-f</code> (als ersten Parameter) kann eine Textdatei angeben werden, welche die Intervalldaten enthält.
+   *                  Mit dem Parameter <code>-f</code> (als ersten Parameter) kann eine Textdatei angeben werden, welche die Intervalldaten enthÃ¤lt.
    */
   public static void main(String[] intervals) 
   {
     DebugMemoryAndTime debugMemTime = new DebugMemoryAndTime();
     debugMemTime.start();
     
-    // Objekt übernimmt das Mergen der Intervalldaten
+    // Objekt Ã¼bernimmt das Mergen der Intervalldaten
     IntervalList myIntervalList = new IntervalList();
-    // Die übergebenen Programm-Argumente initialisieren 
+    // Die Ã¼bergebenen Programm-Argumente initialisieren 
     if ( !myIntervalList.init(intervals) )
     {
-      System.err.println( "Es wurden keine Intervaldaten angegeben. Bitte übergeben Sie die gewünschten Intervallgrenzen als Aufruf-Parameter.");
+      System.err.println( "Es wurden keine Intervaldaten angegeben. Bitte Ã¼bergeben Sie die gewÃ¼nschten Intervallgrenzen als Aufruf-Parameter.");
       return;
     }
     
@@ -61,7 +61,7 @@ public class MergeInterval
   }
 
   /**
-   * Ausgabe der Liste im gewünschtem Zielformat. Dieses entspricht dem Eingabeformat der Intervalldaten.
+   * Ausgabe der Liste im gewÃ¼nschtem Zielformat. Dieses entspricht dem Eingabeformat der Intervalldaten.
    * Die Daten werden nach Standard-Out geschrieben.
    * 
    * @param list Die auszugebende Liste.
